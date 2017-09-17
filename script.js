605b0a08539968503751b5b4c580d73538af4ba9
phiwanczuk <phiwanczuk@gmail.com>
2017-09-17 18:03:45 +0200

    'setbg'

--- script.js
+++ script.js
@@ -1,40 +1,47 @@
-
-
-
 'use strict'
 
-var div = document.querySelector('div');
-var divX = 1300;
-var divY = 300;
+var div = document.querySelector('img');
+var divX = 900;
+var divY = 0;
 var step = 50;
-
-div.style.width = '100px';
-div.style.height = '100px';
 
 div.style.transitionDuration = '.3s';
 
-div.style.position = 'absolute';
-div.style.top = divY+'px';
-div.style.left = divX+'px';
+div.style.position = 'fixed';
+div.style.top = divY + 'px';
+div.style.left = divX + 'px';
 
-function setElementPosition(){
-    div.style.top = divY+'px';
-    div.style.left = divX+'px';
+function trimPx(string){
+    return parseInt(string.replace('px', ''));
 }
 
-function changePosition(x,y){
-    divX = divX + x;
-    divY = divY + y;
+function changeElementPosition(element, xOffset, yOffset) {
+    if (element.style.position !== 'fixed') throw new Error('The element position should be set to fixed!');
+    var xActual = trimPx(div.style.left);
+    var yActual = trimPx(div.style.top);
+    var xNew = xActual + xOffset;
+    var yNew = yActual + yOffset;
+    element.style.top = yNew + 'px';
+    element.style.left = xNew + 'px';
 }
 
-function moveRight(distance){
-    changePosition(distance,0);
-    setElementPosition();
-}
-function moveLeft(distance){
-    moveRight(-distance);
+function moveRight(element, distance) {
+    changeElementPosition(element, distance, 0);
 }
 
-setInterval(function (){
-    moveLeft(20);
-},80);
+function moveLeft(element, distance) {
+    moveRight(element, -distance);
+}
+
+function animateLeft(element) {
+    if (!element) throw new Error('The element should be passed to that functiond!');
+    var interval = setInterval(function () {
+        moveLeft(element, 25);
+        if (trimPx(element.style.left) < (0 - div.offsetWidth)) {
+            clearInterval(interval);
+            element.remove();
+        }
+    }, 100);
+}
+
+animateLeft(div);
