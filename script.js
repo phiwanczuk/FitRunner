3bb4c5d12ecd1e07f717a1fb49f343ebc158d497
phiwanczuk <phiwanczuk@gmail.com>
2017-09-18 15:19:26 +0200

    ninjastyle

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 'use strict'
 
 var div = document.querySelector('.hamburger');
-var divX = 900;
-var divY = 400;
+var divX = 1500;
+var divY = 600;
 var step = 50;
 
 
@@ -37,10 +37,10 @@ function moveLeft(element, distance) {
 }
 
 function animateLeft(element) {
-    if (!element) throw new Error('The element should be passed to that functiond!');
+    if (!element) throw new Error('The element should be passed to that function!');
     var interval = setInterval(function () {
         moveLeft(element, 25);
-        if (trimPx(element.style.left) < (0 - div.offsetWidth)) {
+        if (trimPx(element.style.left) < (325 - div.offsetWidth)) {
             clearInterval(interval);
             element.remove();
         }
@@ -50,8 +50,8 @@ function animateLeft(element) {
 animateLeft(div);
 
 var div = document.querySelector('.pizza');
-var divX = 900;
-var divY = 200;
+var divX = 1500;
+var divY = 600;
 var step = 50;
 
 div.style.transitionDuration = '.3s';
@@ -86,11 +86,59 @@ function animateLeft(element) {
     if (!element) throw new Error('The element should be passed to that functiond!');
     var interval = setInterval(function () {
         moveLeft(element, 25);
-        if (trimPx(element.style.left) < (0 - div.offsetWidth)) {
+        if (trimPx(element.style.left) < (325 - div.offsetWidth)) {
             clearInterval(interval);
             element.remove();
         }
     }, 70);
 }
+
+animateLeft(div);
+
+var div = document.querySelector('.fries');
+var divX = 1500;
+var divY = 600;
+var step = 50;
+
+div.style.transitionDuration = '.3s';
+
+div.style.position = 'fixed';
+div.style.top = divY + 'px';
+div.style.left = divX + 'px';
+
+function trimPx(string){
+    return parseInt(string.replace('px', ''));
+}
+
+function changeElementPosition(element, xOffset, yOffset) {
+    if (element.style.position !== 'fixed') throw new Error('The element position should be set to fixed!');
+    var xActual = trimPx(div.style.left);
+    var yActual = trimPx(div.style.top);
+    var xNew = xActual + xOffset;
+    var yNew = yActual + yOffset;
+    element.style.top = yNew + 'px';
+    element.style.left = xNew + 'px';
+}
+
+function moveRight(element, distance) {
+    changeElementPosition(element, distance, 0);
+}
+
+function moveLeft(element, distance) {
+    moveRight(element, -distance);
+}
+
+function animateLeft(element) {
+    if (!element) throw new Error('The element should be passed to that functiond!');
+    var interval = setInterval(function () {
+        moveLeft(element, 25);
+        if (trimPx(element.style.left) < (325 - div.offsetWidth)) {
+            clearInterval(interval);
+            element.remove();
+        }
+    }, 70);
+}
+
+
 
 animateLeft(div);
